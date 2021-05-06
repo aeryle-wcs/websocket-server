012921a1aba3a09111a65f1809d9d2ed7f8a4568
Aeryle <47262222+Aeryle@users.noreply.github.com>
2021-05-06 10:56:21 +0200

    🐛 FIX: Event for notifications

--- src/main.js
+++ src/main.js
@@ -1,53 +1,71 @@
-const server = require("http").createServer();
-const Logger = require("./utils/Logger");
-const io = require("socket.io")(server, {
-  cors: { origin: "*" },
+const server = require('http').createServer();
+const Logger = require('./utils/Logger');
+const io = require('socket.io')(server, {
+  cors: { origin: '*' }
 });
 
-const { onlineUsers, messages } = require("./utils/CONSTANTS");
+const { onlineUsers, messages } = require('./utils/CONSTANTS');
 const logger = new Logger();
 
-io.on("connection", function connection(socket) {
+const openChats = new Map();
+
+io.on('connection', function connection(socket) {
   console.log(
-    [...io.sockets.sockets].length,
-    [...io.sockets.sockets].filter((s) => s?.data?.username)
+    [ ...io.sockets.sockets ].length,
+    [ ...io.sockets.sockets ].filter((s) => s?.data?.username)
   );
 
   // When the user connects, add him to the list of online users
-  socket.on("user:connect", function userConnect(username) {
-    logger.info(username, "came online");
+  socket.on('user:connect', function userConnect(username) {
+    logger.info(username, 'came online');
 
     socket.data = { username };
     onlineUsers.add(socket.data.username);
-    io.emit("user:connect", [...onlineUsers]);
+    io.emit('user:connect', [ ...onlineUsers ]);
   });
 
-  socket.on("disconnect", function disconnect() {
+  socket.on('disconnect', function disconnect() {
     onlineUsers.delete(socket.data.username);
-    io.emit("user:disconnect", [...onlineUsers]);
+    io.emit('user:disconnect', [ ...onlineUsers ]);
   });
 
-  socket.on("user:disconnect", function userDisconnect() {
-    logger.info(socket.data.username, "went offline");
+  socket.on('user:disconnect', function userDisconnect() {
+    logger.info(socket.data.username, 'went offline');
 
     onlineUsers.delete(socket.data.username);
-    io.emit("user:disconnect", [...onlineUsers]);
+    io.emit('user:disconnect', [ ...onlineUsers ]);
   });
 
-  socket.on("chat:open", function chatOpen(recipient) {
+  socket.on('chat:open', function chatOpen(recipient) {
+    openChats.set(socket.data.username, recipient);
+
     socket.emit(
-      "chat:open",
+      'chat:open',
       messages?.[socket.data.username]?.[recipient] || []
     );
   });
 
-  socket.on("chat:delete", function chatDelete(recipient) {
-    messages[socket.data.username][recipient] = [];
-
-    socket.emit("chat:delete");
+  socket.on('chat:close', function chatClose() {
+    openChats.delete(socket.data.username);
   });
 
-  socket.on("message:create", function messageCreate(recipient, content) {
+  socket.on('chat:delete', function chatDelete(recipient) {
+    messages[socket.data.username][recipient] = [];
+
+    socket.emit('chat:delete');
+  });
+
+  socket.on('message:create', function messageCreate(recipient, content) {
+    let recipientEvent;
+    let startSlice = 0;
+
+    if (openChats.get(recipient) === socket.data.username) {
+      recipientEvent = 'message:create';
+    } else {
+      recipientEvent = 'message:new';
+      startSlice = -1;
+    }
+
     const date = Date.now();
 
     const createMessage = (sender, recipient) => {
@@ -56,9 +74,9 @@ io.on("connection", function connection(socket) {
           {
             from: socket.data.username,
             content,
-            date,
-          },
-        ],
+            date
+          }
+        ]
       };
     };
 
@@ -68,7 +86,7 @@ io.on("connection", function connection(socket) {
       messages[socket.data.username][recipient].push({
         from: socket.data.username,
         content,
-        date,
+        date
       });
     }
 
@@ -78,29 +96,30 @@ io.on("connection", function connection(socket) {
       messages[recipient][socket.data.username].push({
         from: socket.data.username,
         content,
-        date,
+        date
       });
     }
 
-    const recipientSocket = [...io.sockets.sockets].find(
-      ([, s]) => s.data.username === recipient
+    const recipientSocket = [ ...io.sockets.sockets ].find(
+      ([ , s ]) => s.data.username === recipient
     );
 
     socket.emit(
-      "message:create",
-      recipient,
+      'message:create',
       messages[socket.data.username][recipient]
     );
+
+    const slicedMessages = messages[recipient][socket.data.username].slice(startSlice);
+
     if (recipientSocket) {
-      recipientSocket[1].emit("message:new");
       recipientSocket[1].emit(
-        "message:create",
-        messages[recipient][socket.data.username]
+        recipientEvent,
+        recipientEvent === 'message:new' ? slicedMessages[0] : slicedMessages
       );
     }
   });
 
-  socket.on("message:delete", function messageDelete(recipient, index) {
+  socket.on('message:delete', function messageDelete(recipient, index) {
     const removedMessage = messages[socket.data.username][recipient].splice(
       index,
       1
@@ -114,21 +133,24 @@ io.on("connection", function connection(socket) {
       messages[recipient][socket.data.username].splice(recipientIndex, 1);
     }
 
-    const recipientSocket = [...io.sockets.sockets].find(
-      ([, s]) => s.data.username === recipient
+    const recipientSocket = [ ...io.sockets.sockets ].find(
+      ([ , s ]) => s.data.username === recipient
     );
 
     socket.emit(
-      "message:delete",
-      recipient,
+      'message:delete',
       messages[socket.data.username][recipient]
     );
+
     if (recipientSocket) {
       recipientSocket[1].emit(
-        "message:delete",
+        'message:delete',
         messages[recipient][socket.data.username]
       );
     }
+
+    console.log(messages[socket.data.username][recipient]);
+    console.log(messages[recipient][socket.data.username]);
   });
 });
 
